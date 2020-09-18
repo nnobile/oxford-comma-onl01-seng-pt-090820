f6a96cc2177d070e4e57aae339aba04480b490db
Nick Nobile <nickcnobile@gmail.com>
2020-09-18 20:13:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length > 2
     array[-1].insert(0, "and ")
     array.join(", ")
-  elsif array.lenth == 1
+  elsif array.length == 1
     return array
   else return array.insert(" and ")
   end
