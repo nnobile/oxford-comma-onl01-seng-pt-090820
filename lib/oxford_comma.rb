38ba989199480870c9ffbf64d1d3b01f9aa039db
Nick Nobile <nickcnobile@gmail.com>
2020-09-18 01:27:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+return array.join
+end
 
+def oxford_comma(array)
+  return array.join(" and ")
+end
+
+def oxford_comma(array)
+  case array.size
+  when 0
+    ""
+  when 1
+    array.first
+  when 2
+    array.join(" and ")
+  else
+    [array[0..-2].join(", "), array.last].join(", and ")
+  end
 end
