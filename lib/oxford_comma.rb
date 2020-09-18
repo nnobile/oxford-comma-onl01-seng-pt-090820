8d7e6ae4730900ade50521193587630e33a3f78c
Nick Nobile <nickcnobile@gmail.com>
2020-09-18 20:12:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,8 @@ def oxford_comma(array)
   if array.length > 2
     array[-1].insert(0, "and ")
     array.join(", ")
+  elsif array.lenth == 1
+    return array
+  else return array.insert(" and ")  
   end
 end
